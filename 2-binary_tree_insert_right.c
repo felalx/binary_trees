ab7053657d87dfb693a1e707de68368205c2c91a
felalx <ozohfelix@gmail.com>
2024-06-10 12:48:59 -0700

    Greatness

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,4 +1,4 @@
-~#include "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_insert_right - Insert a node as the right-child
@@ -19,17 +19,3 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
-
-	if (parent->right != NULL)
-	{
-		new->right = parent->right;
-		parent->right->parent = new;
-	}
-	parent->right = new;
-
-	return (new);
-}
